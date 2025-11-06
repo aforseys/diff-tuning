fd7551ec35d6f5b11bd3571e6b57244ac87e016b
Alexandra Forsey <aforsey@mit.edu>
2025-11-06 13:04:43 -0500

    Visualizing learned gmm energy

---
+++ itps/scripts/eval_gmm.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+
+# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import torch
+import logging 
+from pathlib import Path
+from matplotlib import pyplot as plt
+from datetime import datetime as dt
+from torch import Tensor, nn
+from itps.common.policies.factory import make_policy
+from itps.common.datasets.factory import make_dataset
+from itps.common.envs.factory import make_env
+from itps.common.utils.utils import get_safe_torch_device, init_hydra_config, init_logging, set_global_seed
+
+def gen_obs(conditional, N):
+    "generates a batch object that matches same type as passed through model, only contains obs"
+
+    observations=[]
+    for i in range(1 if not conditional else 3):
+        obs_tensor = torch.full((N, 1, 1), i, device=torch.device("cuda"))
+        obs_dict= {
+            'observation.state':obs_tensor, 
+            'observation.environment_state':obs_tensor
+        }
+        observations.append(obs_dict)
+    return observations
+
+
+def gen_xy_grid(x_range, y_range):
+
+    xmin,xmax=x_range
+    ymin,ymax=y_range
+    
+    xx, yy = np.meshgrid(
+    np.linspace(xmin, xmax, 200),
+    np.linspace(ymin, ymax, 200)
+    )
+
+    grid = np.column_stack([xx.ravel(), yy.ravel()]) 
+    trajs = torch.tensor(grid, device=torch.device("cuda")).unsqueeze(dim=1) 
+    return trajs
+
+
+def run_inference(policy, N=100, conditional=False, return_energy=False):
+
+    obs = gen_obs(conditional=conditional, N=N)
+
+    inference_output = []
+    for o in obs: 
+        if return_energy: 
+            actions, energy = policy.run_inference(o, return_energy=return_energy)
+            inference_output.append((actions.detach().cpu().numpy(), energy.detach().cpu().numpy())) 
+
+        actions = policy.run_inference(o, return_energy=return_energy)
+        inference_output.append(actions)
+
+    #TODO: REMOVE FROM DEVICE CORRECTLY
+    return inference_output
+
+def eval_energy(policy, trajs, conditional=False):
+
+    obs = gen_obs(conditional=conditional, N=len(trajs))
+    global_conds = [policy._prepare_global_cond(o) for o in obs]
+    energies = []
+    for gc in global_conds:
+        energy = policy.get_traj_energies(trajectories=trajs, global_cond=gc)
+        energies.append(energy.detach().cpu().numpy())
+    return energies 
+
+def vis_inference(policy, conditional, N, x_range=(-8, 8), y_range=(-8,8)):
+
+    trajs = gen_xy_grid(x_range=x_range, y_range=y_range)
+    energies = eval_energy(policy, trajs, conditional=conditional)
+    inference_output = run_inference(policy, N=N, conditional=conditional)
+
+    print(trajs.shape)
+    print(energies.shape)
+    print(inference_output.shape)
+    assert False
+
+    #plot all energy landscapes in list given trajs
+    for i in range(len(energies)):
+        #plot 
+        x,y = trajs
+        z = energies[i]
+        if conditional:
+            title = "Energy landscape conditioned on cluster observation {i}"
+        else:
+            title = "Energy landscape (unconditional)"
+
+        plt.figure(i)
+        plt.contourf(xx, yy, zz, levels=20)
+        # plot where sampled points are with x's 
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.title(title)
+  
+        
+def vis_energy_landscape(policy, conditional, x_range=(-8, 8), y_range=(-8,8)):
+
+    trajs = gen_xy_grid(x_range=x_range, y_range=y_range)
+    energies = eval_energy(policy, trajs, conditional=conditional)
+
+    print(trajs.shape)
+    print(energies.shape)
+
+    assert False
+
+    #plot all energy landscapes in list given trajs
+    for i in range(len(energies)):
+        #plot 
+        x,y = trajs
+        z = energies[i]
+        if conditional:
+            title = "Energy landscape conditioned on cluster observation {i}"
+        else:
+            title = "Energy landscape (unconditional)"
+
+        plt.figure(i)
+        plt.axes(projection="3d").plot_surface(x, y, z, cmap="viridis", edgecolor="none")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.title(title)
+
+def main(
+    pretrained_policy_path: Path | None = None,
+    hydra_cfg_path: str | None = None,
+    out_dir: str | None = None,
+    config_overrides: list[str] | None = None,
+    conditional: bool | False = False
+):
+    assert (pretrained_policy_path is None) ^ (hydra_cfg_path is None)
+    if pretrained_policy_path is not None:
+        hydra_cfg = init_hydra_config(str(pretrained_policy_path / "config.yaml"), config_overrides)
+    else:
+        hydra_cfg = init_hydra_config(hydra_cfg_path, config_overrides)
+
+    if out_dir is None:
+        out_dir = f"outputs/eval/{dt.now().strftime('%Y-%m-%d/%H-%M-%S')}_{hydra_cfg.env.name}_{hydra_cfg.policy.name}"
+
+    # Check device is available
+    device = get_safe_torch_device(hydra_cfg.device, log=True)
+
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cuda.matmul.allow_tf32 = True
+    set_global_seed(hydra_cfg.seed)
+
+    logging.info("Making policy.")
+    if hydra_cfg_path is None:
+        policy = make_policy(hydra_cfg=hydra_cfg, pretrained_policy_name_or_path=str(pretrained_policy_path))
+    else:
+        # Note: We need the dataset stats to pass to the policy's normalization modules.
+        policy = make_policy(hydra_cfg=hydra_cfg, dataset_stats=make_dataset(hydra_cfg).stats)
+
+    assert isinstance(policy, nn.Module)
+
+
+    vis_energy_landscape(policy, conditional)
+    vis_inference(policy, conditional, N=500)
