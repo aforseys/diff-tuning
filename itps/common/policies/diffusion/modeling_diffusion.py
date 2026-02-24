0de4b81f7280394a7b6f80216b6d0aa300629ca1
Alexandra Forsey <aforsey@mit.edu>
2026-02-24 17:03:39 -0500

    Updated config file attributes for evaluation purposes

--- itps/common/policies/diffusion/modeling_diffusion.py
+++ itps/common/policies/diffusion/modeling_diffusion.py
@@ -96,6 +96,9 @@ class DiffusionPolicy(nn.Module, PyTorchModelHubMixin):
         self.use_env_state = "observation.environment_state" in config.input_shapes
         self.use_goal_cond = "episode_goal" in config.input_shapes
 
+        # Ensure config type matches state input
+        assert ((self.use_goal_cond and config.input_type.lower()=='conditional') or ((not self.use_goal_cond) and (not (config.input_type.lower() == 'conditional')))), "Condition type doesn't match episode goal inclusion"
+    
     @property
     def n_obs_steps(self) -> int:
         return self.config.n_obs_steps
