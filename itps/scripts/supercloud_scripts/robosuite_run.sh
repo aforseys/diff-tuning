797c19797e66ebccc20fb2276ff73ff6601897b7
Alexandra Forsey <aforsey@mit.edu>
2026-05-24 21:37:36 -0400

    Robosuite mulistrategy supercloud run files

---
+++ itps/scripts/supercloud_scripts/robosuite_run.sh
@@ -0,0 +1,19 @@
+# Initialize the module command first source
+source /etc/profile
+
+# Load Anaconda Module
+module load conda/Python-ML-2026a-pytorch
+module load cuda/13.1
+
+export PYTHONPATH="/home/gridsan/aforsey/diff-tuning:$PYTHONPATH"
+export WANDB_MODE=offline
+
+# ── Configuration ─────────────────────────────────────────────────────────────
+CONFIGS_DIR="/home/gridsan/aforsey/diff-tuning/itps/configs/policy/robosuite/robosuite_strategy_gc_dp"    # Directory containing generated run_*.yaml files
+SCRIPT="scripts/train.py"             # The python training script
+ENV_NAME="robosuite"                # env={ENV_NAME} passed to the script
+# ──────────────────────────────────────────────────────────────────────────────
+
+python scripts/train.py \
+    --configs_dir $CONFIGS_DIR \
+    --env $ENV_NAME
