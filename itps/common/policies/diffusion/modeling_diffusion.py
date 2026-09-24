66eebb945226bd6aa3e919148ed3284a4e50e623
Alexandra Forsey <aforsey@mit.edu>
2026-09-24 18:04:43 -0400

    Original sampling combination with noise

--- itps/common/policies/diffusion/modeling_diffusion.py
+++ itps/common/policies/diffusion/modeling_diffusion.py
@@ -638,19 +638,19 @@ class EBMDiffusionModel(DiffusionModel):
                 grad_history[t.item()]=[]
 
             # Repredict sample from denoised estimate
-            # if denoise:
-            #     pred_noise = self.model(sample, batched_t, global_cond=global_cond)
-            #     x0_hat = (sample - torch.sqrt(1-alpha_bar_t)*pred_noise)/torch.sqrt(alpha_bar_t)
-            #     sample_new = (torch.sqrt(alpha_bar_t) * x0_hat).detach()
-            #     # sample_new = torch.clamp(torch.sqrt(alpha_bar_t) * x0_hat, -max_val, max_val).detach()
+            if denoise:
+                pred_noise = self.model(sample, batched_t, global_cond=global_cond)
+                x0_hat = (sample - torch.sqrt(1-alpha_bar_t)*pred_noise)/torch.sqrt(alpha_bar_t)
+                sample_new = (torch.sqrt(alpha_bar_t) * x0_hat).detach()
+                # sample_new = torch.clamp(torch.sqrt(alpha_bar_t) * x0_hat, -max_val, max_val).detach()
 
-            #     if return_grad_steps:
-            #         # Undo the timestep scaling so callers only need to unnormalize.
-            #         grad_history[t.item()].append({
-            #             'pos': (sample / torch.sqrt(alpha_bar_t)).detach().clone(),
-            #             'next_pos': (sample_new / torch.sqrt(alpha_bar_t)).detach().clone()
-            #         })
-            #     sample=sample_new
+                if return_grad_steps:
+                    # Undo the timestep scaling so callers only need to unnormalize.
+                    grad_history[t.item()].append({
+                        'pos': (sample / torch.sqrt(alpha_bar_t)).detach().clone(),
+                        'next_pos': (sample_new / torch.sqrt(alpha_bar_t)).detach().clone()
+                    })
+                sample=sample_new
 
             t_idx = (self.noise_scheduler.timesteps == t).nonzero().item()                                                                      
             if opt_subset is None or t_idx >= (total_timesteps - opt_subset): #skip optimization in landscapes if specified 
